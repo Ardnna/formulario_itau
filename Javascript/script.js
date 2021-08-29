890ccb24e0170b1f7d246807a487d1ac5f8725c7
Andréia Michaelsen <michaelsen.andreia@gmail.com>
2021-08-28 22:28:03 -0300

    confirmar

--- Javascript/script.js
+++
@@ -1,9 +0,0 @@
-function enviar() {
-
-    var nome = document.getElementById("nome");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + '! Seu cadastro foi realizado com sucesso.');
-        }
-        }
-        
---
+++ script.js
@@ -0,0 +1,8 @@
+        function enviar() {
+            var nome = document.getElementById("nome");
+        
+            if (nome.value != "") {
+                alert('Obrigado sr(a) ' + nome.value + '! Seu cadastro foi realizado com sucesso.');
+            }
+        }
+        

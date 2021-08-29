1eb188582ed1e1a1d3ea887be64371880b7cce97
Andréia Michaelsen <michaelsen.andreia@gmail.com>
2021-08-28 22:34:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
         
             if (nome.value != "") {
                 alert('Obrigado sr(a) ' + nome.value + '! Seu cadastro foi realizado com sucesso.');
+                window.location.href='https://sleepy-fermat-45ba7d.netlify.app/';
             }
         }
         
